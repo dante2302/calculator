ec4e7a1d05be81327f94a92a67efc11cf60a1561
Dante <guchoo923@gmail.com>
2022-04-19 21:12:54 +0300

    Add animation, audio, new eventListeners
        Add all audio files to audio folder, bind them to eventListeners
        for click and keydown events. Add an animation onclick and onkeydown.

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-//EXTRA CREDIT: Add a . button and let users input decimals!(disable the decimal button if there’s already one in the display)
-// EXTRA CREDIT: Add a “backspace” button, so the user can undo if they click the wrong number.
 // EXTRA CREDIT: Add keyboard support!
 
 
 const operators = Array.from(document.querySelectorAll('.operator'));
-const numberButtons = Array.from(document.querySelectorAll('.number'));
+const numberButtons = Array.from(document.querySelectorAll('.number')).reverse();
 const operateButton = document.querySelector(".operate");
 const clearButton = document.querySelector('.clear');
 const deleteButton = document.querySelector('.delete');
 const floatButton = document.querySelector('.float');
 const display = document.querySelector('.display');
 const history = document.querySelector('.history');
-
+ 
+console.log(operators)
 let currentNumber1 = null
 let currentNumber2 = null
 let currentOperator = '';
@@ -27,13 +26,13 @@ function resetScreen(){
     shouldResetScreen = false
 }
 
-function deleteChar(){
+function deleteChar(e){
     if(display.textContent == null)return;
     let newStr = display.textContent.slice(0,-1);
     display.textContent = newStr
 }
 
-function clear(){
+function clear(e){
     currentNumber1 = null;
     history.textContent='';
     display.textContent='';
@@ -41,7 +40,8 @@ function clear(){
     shouldResetScreen = false;
 }
 
-function addFloat(){
+function addFloat(e){
+    if (shouldResetScreen) resetScreen()
     if(display.textContent == null || display.textContent.includes('.'))return
     else display.textContent += '.';
 }
@@ -56,7 +56,7 @@ function setCurrentOperator(sign){
 }
 
 function operate(){
-    if(currentOperator==null || shouldResetScreen)return
+    if(currentOperator==null || shouldResetScreen || currentNumber1 == null)return
 
     currentNumber2 = parseFloat(display.textContent);
 
@@ -65,7 +65,6 @@ function operate(){
         display.textContent = "You cannot divide by zero";
         shouldResetScreen = true;
     }
-
     let result = null;
 
     switch(currentOperator){
@@ -73,7 +72,6 @@ function operate(){
         case "-" : result = subtract(currentNumber1,currentNumber2); break;
         case "*" : result = multiply(currentNumber1,currentNumber2); break;
         case "/" : result = divide(currentNumber1,currentNumber2); break;
-        default : if(num2==null){break}
     }
     result  = round(result);
     display.textContent = result;
@@ -110,20 +108,72 @@ function typeNum(value){
 
 numberButtons.forEach((button) => button.addEventListener('click', () => typeNum(button.textContent)))
 operators.forEach((button) => button.addEventListener('click', () => setCurrentOperator(button.textContent)))
-clearButton.addEventListener("click", () => clear());
-deleteButton.addEventListener("click", () => deleteChar());
-floatButton.addEventListener("click",() => addFloat());
-operateButton.addEventListener("click", () => operate());
+clearButton.addEventListener("click", () =>  {playSoundOnClick(27); clear()})
+deleteButton.addEventListener("click",()  => {playSoundOnClick(8);deleteChar()})
+floatButton.addEventListener("click",() => {playSoundOnClick(190);addFloat()});
+operateButton.addEventListener("click", () => {playSoundOnClick(13);operate()});
 
+// Adding sound manually to buttons because every button plays a different sound
+
+
+numberButtons[0].addEventListener('click', ()=> playSoundOnClick(48)) //0
+numberButtons[3].addEventListener('click', ()=> playSoundOnClick(49)) //1
+numberButtons[2].addEventListener('click', ()=> playSoundOnClick(50)) //2
+numberButtons[1].addEventListener('click', ()=> playSoundOnClick(51)) //3
+numberButtons[6].addEventListener('click', ()=> playSoundOnClick(52)) //4
+numberButtons[5].addEventListener('click', ()=> playSoundOnClick(53)) //5
+numberButtons[4].addEventListener('click', ()=> playSoundOnClick(54)) //6
+numberButtons[9].addEventListener('click', ()=> playSoundOnClick(55)) //7
+numberButtons[8].addEventListener('click', ()=> playSoundOnClick(56)) //8
+numberButtons[7].addEventListener('click', ()=> playSoundOnClick(57))
+
+operators[0].addEventListener('click', ()=> playSoundOnClick(191));
+operators[1].addEventListener('click', ()=> playSoundOnClick(48))
+operators[2].addEventListener('click', ()=> playSoundOnClick(189));
+operators[3].addEventListener('click', ()=> playSoundOnClick(187));
+
+//Keyboard support
+
+window.addEventListener('keydown',handleKeyboard);
+
+function handleKeyboard(e){
+        playSound(e)
+        if (e.key >= 0 && e.key <= 9) typeNum(e.key)
+        if(e.key == ".") addFloat();
+        if(e.key === "=" || e.key === "Enter") operate();
+        if(e.key === "Backspace") deleteChar();
+        if(e.key === "Escape") clear()
+        if (e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/') setCurrentOperator(e.key)
+}
+
+// Add sound effects
+
+function playSound(e){
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const button = document.querySelector(`button[data-key="${e.keyCode}"]`)
+    if(!audio) return;
+    audio.currentTime = 0
+    audio.play();
+    button.classList.add('playing');
+}
+
+function playSoundOnClick(e){
+    const audio = document.querySelector(`audio[data-key="${e}"]`)
+    const button = document.querySelector(`button[data-key="${e}"]`)
+    if(!audio) return;
+    audio.currentTime = 0
+    audio.play();
+    button.classList.add('playing');
+}
 // Add animations to buttons
 
-        // function removeTransition(e){
-        //     if(e.propertyName != 'transform') return
-    
-        //     this.classList.remove('playing');
-        // }
-    
-        // const keys = document.querySelectorAll(".key")
-        // keys.forEach(key => {
-        //     key.addEventListener('transitionend', removeTransition)
-        // });
+function removeTransition(e){
+    if(e.propertyName != 'transform') return
+    this.classList.remove('playing');
+    }
+    const buttons = document.querySelectorAll("button")
+    buttons.forEach(button => {
+    button.addEventListener('transitionend', removeTransition)
+});
+
+
